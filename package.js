2b645b2e77e08fe7bcd94fe22d34e0c68dc13d3e
David Robbins <robbins.david@gmail.com>
2015-06-18 09:34:59 -0500

    After initial publish

--- package.js
+++ package.js
@@ -13,7 +13,7 @@ Package.onUse(function(api) {
     api.versionsFrom("1.1.0.2");
     api.use("coffeescript");
     api.use(["standard-app-packages", "templating", "blaze", "ui", "reactive-var", "reactive-dict"]);
-    api.use("harrison:papa-parse");
+    api.use("harrison:papa-parse@1.1.0");
     if (api.export) api.export("BulkInterface");
     api.addFiles(["bulk-interface-templates.html", "bulk-interface.css"], "client");
     api.addFiles(["lib/jquery.dataTables.min.js", "lib/dataTables.bootstrap.css", "lib/dataTables.bootstrap.js"], "client")
